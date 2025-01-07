93972b9976403d1781f85f1f9283c683f8479ee5
wumpfroot <itswumpa@gmail.com>
2025-01-07 03:15:14 +0200

    Basic get-request working

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def home():
     return "<h1>Home</h1>"
 
+@app.route("/get-game/<game_id>")
+def get_game(game_id):
+    game_data = {
+        "game_id": game_id,
+        "title": "Balatro",
+        "developer": "LocalThunk"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        game_data["extra"] = extra
+
+    return jsonify(game_data), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
